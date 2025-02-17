270994de81671f92daded06ca58292dbae0d725a
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2025-02-17 09:22:13 +0500

    Create Q2.c

---
+++ LABS/Lab4/Q2/Q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        for (int i = 0; i < 100; i++)
+            printf("I am a child process\n");
+    } else {
+        for (int i = 0; i < 100; i++)
+            printf("I am a parent process\n");
+    }
+    return 0;
+}
